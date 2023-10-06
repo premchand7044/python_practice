35284092e3dfd4afbc9e6e5036cb5e2e91b2e3aa
premchand7044 <139372647+premchand7044@users.noreply.github.com>
2023-10-06 17:24:54 +0530

    Add files via upload

---
+++ STRINGS.py
@@ -0,0 +1,11 @@
+str="Hi How R U "
+print(str)
+str=input()
+print(str)
+for i in range(len(str)):
+    print(i,str[i])
+str2='hello'
+str3='python'
+str4=(str2+str3)
+print(str4)
+print(str4*3)
---
+++ string_functions.py
@@ -0,0 +1,19 @@
+#string built in and functions
+a='hello'
+print(a)
+print(a.center(11,'X'))
+b= 'Hi How are You'
+print(b.count('o',0,len(b)))
+c = 'Welcome to python'
+print(c.endswith('n', 0, len(c)))
+d='Premchand'
+e=d.find("d",0,len(d))
+print(e)
+print(d.isupper())
+print(d.islower())
+print(d.istitle())
+print(len(d))
+f=max(d)
+print(f)
+g=min(a)
+print(g)
